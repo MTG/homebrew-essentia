4ae760622c49cc757c18f2fc532201eedfe30ff0
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2018-11-29 18:07:35 +0100

    Support for both python2 and python3 and formula updates
    - Support building for both python3 and python2 (provided by Homebrew)
    - New build flags to specify for which python to build or whether to make python
      dependency optional (may be useful for those who just want to use the C++ library,
      or command-line extractors without messing with python)
    - Make the formula head-only to avoid build errors (the --only-python flag was
      introduced after the latest 2.1-beta4 release)
    - Dependent libraries are now required, not optional
    - Remove some unnecessary dependencies
    - Add build tests

--- essentia.rb
+++ essentia.rb
@@ -1,78 +1,93 @@
-require 'formula'
-
 class Essentia < Formula
   desc "Library for audio analysis and audio-based music information retrieval"
   homepage "http://essentia.upf.edu"
-  url "https://github.com/MTG/essentia/archive/v2.1_beta3.tar.gz"
-  version "2.1_beta3"
-  sha256 "974b90af6093fd12d6c9ac370f13b0dd1ecf80a418670bf35582ed5d95d65237"
-
   head 'https://github.com/MTG/essentia.git'
 
-  depends_on "pkg-config"
-  depends_on "gcc"
-  depends_on "readline"
-  depends_on "sqlite"
-  depends_on "gdbm"
-  depends_on "freetype"
-  depends_on "libpng"
-  depends_on "python"
-  #depends_on :python
-  depends_on "numpy"
-  depends_on "libyaml" => :recommended
-  depends_on "fftw" => :recommended
-  depends_on "ffmpeg" => :recommended
-  depends_on "libsamplerate" => :recommended
-  depends_on "libtag" => :recommended
-  depends_on "chromaprint" => :recommended
+  include Language::Python::Virtualenv
+
+  depends_on "pkg-config" => :build
+  depends_on "gcc" => :build
+  depends_on "libyaml"
+  depends_on "fftw"
+  depends_on "ffmpeg"
+  depends_on "libsamplerate"
+  depends_on "libtag"
+  depends_on "chromaprint"
   depends_on "gaia" => :optional
 
+  option "without-python", "Build without python3 support"
+  option "without-python@2", "Build without python2 support"
+
+  depends_on "python" if build.with? "python"
+  depends_on "python@2" if build.with? "python@2"
+
+  depends_on "numpy" if build.with? "python@2" and build.with? "python"
+  depends_on "numpy" => "--without-python" if build.with? "python@2" and !build.with? "python"
+  depends_on "numpy" => "--without-python@2" if build.with? "python" and !build.with? "python@2"
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/16/d8/bc6316cf98419719bd59c91742194c111b6f2e85abac88e496adefaf7afe/six-1.11.0.tar.gz"
+    sha256 "70e8a77beed4562e7f14fe23a786b54f6296e34344c23bc42f07b15018ff98e9"
+  end
+
   def install
+
+    build_flags = [
+      "--mode=release",
+      "--with-examples",
+      "--with-vamp",
+      "--prefix=#{prefix}"
+    ]
+
     if build.with? "gaia"
-      system "./waf", "configure", "--mode=release", 
-                                   "--with-python", 
-                                   "--with-vamp",
-                                   "--with-gaia",
-                                   "--with-examples",
-                                   "--prefix=#{prefix}"
+      system "./waf", "configure", "--with-gaia", *build_flags
     end
     if build.without? "gaia"
-      system "./waf", "configure", "--mode=release", 
-                                   "--with-python", 
-                                   "--with-vamp",
-                                   "--with-examples",
-                                   "--prefix=#{prefix}"
+      system "./waf", "configure", *build_flags
     end
     system "./waf"
     system "./waf", "install"
+
+    python_flags = [
+      "--mode=release",
+      "--only-python",
+      "--pkg-config-path=#{prefix}/lib/pkgconfig/",
+      "--prefix=#{prefix}"
+    ]
+
+    if build.with? "python"
+      system "python3", "./waf", "configure", *python_flags
+      system "python3", "./waf"
+      system "python3", "./waf", "install"
+    end
+    if build.with? "python@2"
+      system "python2", "./waf", "configure", *python_flags
+      system "python2", "./waf"
+      system "python2", "./waf", "install"
+
+      virtualenv_install_with_resources :using => "python@2"
+      essentia_path = libexec/"lib/python2.7/site-packages"
+      pth_contents = "import site; site.addsitedir('#{essentia_path}')\n"
+      (lib/"python2.7/site-packages/homebrew-essentia.pth").write pth_contents
+    end
   end
 
-  def caveats
-"""
-You may be also interested in installing ipython and matplotlib python packages.
+  test do
+    system "#{bin}/essentia_streaming_extractor_music",
+           "/System/Library/Sounds/Glass.aiff",
+           "Glass.json"
 
-You can install these using
-   pip install ipython matplotlib
-Or using the installation method you prefer.
-"""
+    py_test = <<~EOS
+      import essentia.standard as estd
+      import essentia.streaming as estr
+      estd.MusicExtractor()("/System/Library/Sounds/Glass.aiff")
+    EOS
+
+    if build.with? "python"
+      system "python3", "-c", "#{py_test}"
+    end
+    if build.with? "python@2"
+      system "python2", "-c", "#{py_test}"
+    end
   end
-
-  bottle do
-    cellar :any
-    sha256 "ded4b3524618ca4fe38a940f09a629cc95b853d808ca174f5da21984922ac435" => :yosemite
-  end
-
-
-#  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test essentia`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-#    system "false"
-#  end
 end
