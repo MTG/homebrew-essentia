26b0b9328b140964958fea5b5703c131560e73ea
Amar1729 <amar.paul16@gmail.com>
2023-09-24 08:30:03 -0400

    numpy: extract at 1.23.3

---
+++ numpy@1.23.3.rb
@@ -0,0 +1,58 @@
+class NumpyAT1233 < Formula
+  desc "Package for scientific computing with Python"
+  homepage "https://www.numpy.org/"
+  url "https://files.pythonhosted.org/packages/0a/88/f4f0c7a982efdf7bf22f283acf6009b29a9cc5835b684a49f8d3a4adb22f/numpy-1.23.3.tar.gz"
+  sha256 "51bf49c0cd1d52be0a240aa66f3458afc4b95d8993d2d04f0d91fa60c10af6cd"
+  license "BSD-3-Clause"
+  head "https://github.com/numpy/numpy.git", branch: "main"
+
+  depends_on "gcc" => :build # for gfortran
+  depends_on "libcython" => :build
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
+  depends_on "openblas"
+
+  fails_with gcc: "5"
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
+        .sort_by(&:version) # so that `bin/f2py` and `bin/f2py3` use python3.10
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
+
+  def install
+    openblas = Formula["openblas"]
+    ENV["ATLAS"] = "None" # avoid linking against Accelerate.framework
+    ENV["BLAS"] = ENV["LAPACK"] = openblas.opt_lib/shared_library("libopenblas")
+
+    config = <<~EOS
+      [openblas]
+      libraries = openblas
+      library_dirs = #{openblas.opt_lib}
+      include_dirs = #{openblas.opt_include}
+    EOS
+
+    Pathname("site.cfg").write config
+
+    pythons.each do |python|
+      site_packages = Language::Python.site_packages(python)
+      ENV.prepend_path "PYTHONPATH", Formula["libcython"].opt_libexec/site_packages
+
+      system python, "setup.py", "build", "--fcompiler=#{Formula["gcc"].opt_bin}/gfortran",
+                                          "--parallel=#{ENV.make_jobs}"
+      system python, *Language::Python.setup_install_args(prefix, python)
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      system python, "-c", <<~EOS
+        import numpy as np
+        t = np.ones((3,3), int)
+        assert t.sum() == 9
+        assert np.dot(t, t).sum() == 27
+      EOS
+    end
+  end
+end
